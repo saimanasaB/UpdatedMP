36b00bd2f5e72390000044147b7b5736c448bbb9
Sai Manasa B <91835638+saimanasaB@users.noreply.github.com>
2024-08-17 22:52:30 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,7 +15,7 @@ def get_base64_of_image(image_file):
         return base64.b64encode(img.read()).decode()
 
 # Path to your image file
-image_path = "inflation7.jpg"
+image_path = "in9.jpg"
 
 # Convert the image to a Base64 string
 img_base64 = get_base64_of_image(image_path)
