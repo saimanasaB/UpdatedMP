8801b28d2f620400d3560b0fc6b501a26d7d270a
Sai Manasa B <91835638+saimanasaB@users.noreply.github.com>
2024-08-17 17:23:53 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -365,4 +365,4 @@ elif page == "Contact Us":
 
     Thank you for your interest in connecting with us!
     </div>
-    """, unsafe_allow_html=True)                                                                                                              im not liking the CSS which are font size and colour of about us and contact us page could you please make it more attractive 
+    """, unsafe_allow_html=True)                                                                                                              im not liking the CSS which are font size and colour of about us and contact us page could you please make it more attractive      
